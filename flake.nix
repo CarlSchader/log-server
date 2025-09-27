9865e0f0efaf7c41f1b9949ebaae4644c20259ae
carlschader <carlschader@gmail.com>
2025-09-27 14:49:17 -0400

    working nix package

--- flake.nix
+++ flake.nix
@@ -10,6 +10,29 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.default = pkgs.python312.pkgs.buildPythonPackage {
+          pname = "log-server";
+          version = "0.1.0";
+          src = ./.;
+          pyproject = true;
+
+          propagatedBuildInputs = with pkgs.python312.pkgs; [
+            fastapi
+            pyjwt
+            uvicorn
+          ];
+
+          build-system = with pkgs.python312.pkgs; [
+            setuptools
+            wheel
+          ];
+        };
+
+        apps."${system}".default = {
+           type = "app";
+           protram = "${self.packages.${system}.default}/bin/log-server";
+        };
+
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             uv
